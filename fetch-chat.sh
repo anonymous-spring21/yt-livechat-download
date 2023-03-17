49278cf4ff0e006a20f2c0d25f9f9b90a0224157
anonyMous <anonymous.spring21@gmail.com>
2023-03-18 00:50:23 +0800

    feat: fetch script skip existing video

--- fetch-chat.sh
+++ fetch-chat.sh
@@ -6,5 +6,12 @@ YOUTUBE_URL_PREFIX='https://www.youtube.com/watch?v='
 ARG1=$1
 BLANK=""
 YOUTUBE_VIDEO_ID="${ARG1/$YOUTUBE_URL_PREFIX/$BLANK}"
-echo "Using youtube video id ($YOUTUBE_VIDEO_ID)..."
-chat_downloader https://www.youtube.com/watch?v=$YOUTUBE_VIDEO_ID > data/$YOUTUBE_VIDEO_ID.livechat.txt
+DOWNLOAD_FILE="data/$YOUTUBE_VIDEO_ID.livechat.txt"
+echo "Downloading youtube video id ($YOUTUBE_VIDEO_ID) to file($DOWNLOAD_FILE)..."
+
+if [ -f "$DOWNLOAD_FILE" ]; then
+    echo "$DOWNLOAD_FILE already exists."
+else 
+    chat_downloader https://www.youtube.com/watch?v=$YOUTUBE_VIDEO_ID > data/$YOUTUBE_VIDEO_ID.livechat.txt
+fi
+
